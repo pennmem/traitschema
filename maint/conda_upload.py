35284f47f671bec92e25e101355815e3cfc304d4
James Bruska <jamesbthecoder@gmail.com>
2022-12-12 15:14:29 -0500

    Added conda upload script

---
+++ maint/conda_upload.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import glob
+import shlex
+from subprocess import check_call
+
+for platform in ['linux-64', 'osx-64', 'osx-arm64', 'win-32', 'win-64']:
+    files = glob.glob('build/{}/*.tar.bz2'.format(platform))
+    cmds = ['anaconda upload -u pennmem {}'.format(f) for f in files]
+    for cmd in cmds:
+        print(cmd)
+        check_call(shlex.split(cmd))
