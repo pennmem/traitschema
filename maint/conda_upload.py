22c3997c1011540c022b7a5a7de4a327c7f296b8
James Bruska <jamesbthecoder@gmail.com>
2023-03-28 11:56:50 -0400

    Update conda_upload architectures

--- maint/conda_upload.py
+++ maint/conda_upload.py
@@ -4,7 +4,7 @@ import glob
 import shlex
 from subprocess import check_call
 
-for platform in ['linux-64', 'osx-64', 'osx-arm64', 'win-32', 'win-64']:
+for platform in ['linux-64', 'linux-aarch64', 'osx-64', 'osx-arm64', 'win-64']:
     files = glob.glob('build/{}/*.tar.bz2'.format(platform))
     cmds = ['anaconda upload -u pennmem {}'.format(f) for f in files]
     for cmd in cmds:
