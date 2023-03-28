3a4c470a1dabc01719afb2bd4cdeca6b934d2436
James Bruska <jamesbthecoder@gmail.com>
2023-03-28 10:11:28 -0400

    Change deploy script

---
+++ maint/deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# This script gets run on TravisCI to deploy new releases. It gets triggered
+# on tagged versions only, so make sure to tag when releasing!
+set -ex
+
+VERSION=`python -c "print(''.join('$PYTHON_VERSION'.split('.')))"`
+anaconda -t $ANACONDA_TOKEN upload build/**/$PKG_NAME*-py$VERSION*.tar.bz2
